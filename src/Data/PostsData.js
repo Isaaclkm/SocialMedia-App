f8ea47f650a341389d426f8ce33c388a1939e6f2
Isaaclkm <isaacfelixmatos@gmail.com>
2022-11-06 22:19:35 -0500

    added things

---
+++ src/Data/PostsData.js
@@ -0,0 +1,29 @@
+import postPic1 from '../img/postpic1.jpg'
+import postPic2 from '../img/postpic2.jpg'
+import postPic3 from '../img/postpic3.JPG'
+
+
+
+export const PostsData = [
+    {
+        img: postPic1, 
+        name: 'Tzuyu',
+        desc: 'Happy new Year all friends! #2023',
+        likes: 2300,
+        liked: true
+    },
+    {
+        img: postPic2, 
+        name: 'Selena Gomez',
+        desc: 'Art Galery',
+        likes: 800,
+        liked: true
+    },
+    {
+        img: postPic3, 
+        name: 'Maryam',
+        desc: 'Party time',
+        likes: 380,
+        liked: false
+    },
+]
--- src/components/PostShare/PostShare.jsx
+++ src/components/PostShare/PostShare.jsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import React from 'react';
+import ProfileImage from '../../img/profileImg.jpg';
+import './PostShare.css';
+import {UilScenery} from '@iconscout/react-unicons'
+import {UilPlayCircle} from '@iconscout/react-unicons';
+import {UilLocationPoint} from '@iconscout/react-unicons';
+import {UilSchedule} from '@iconscout/react-unicons';
+
 
 const PostShare = () => {
   return (
-    <div>PostShare</div>
+    <div className="PostShare">
+        <img src={ProfileImage} alt="" />
+        <div>
+            <input type="text" 
+            placeholder="What's happening"/>
+            <div className="postOptions">
+                <div className="option"
+                style={{color: "var(--photo)"}}
+                >
+                    <UilScenery/>Photo
+                </div>
+                <div className="option"
+                style={{color: "var(--video)"}}>
+                    <UilPlayCircle/> Video
+                </div>
+                <div className="option"
+                style={{color: "var(--location)"}}>
+                    <UilLocationPoint/> Location
+                </div>
+                <div className="option"
+                style={{color: "var(--schedule)"}}>
+                    <UilSchedule/> Schedule
+                </div>
+                <button className='button ps-button'>
+                    Share
+                </button>
+
+            </div>
+        
+        </div>
+    </div>
   )
 }
 
