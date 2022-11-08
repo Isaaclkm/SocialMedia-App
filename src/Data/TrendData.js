c975ebb36331b090460b156df8bc202a8ad0d19b
Isaaclkm <isaacfelixmatos@gmail.com>
2022-11-07 22:24:23 -0500

    added the right sie

---
+++ src/Data/TrendData.js
@@ -0,0 +1,22 @@
+export const TrendData = [
+    {
+        name: 'Avengers',
+        shares: 91
+    },
+    {
+        name: 'Barbie',
+        shares: 82
+    },
+    {
+        name: 'Link',
+        shares: 80
+    },
+    {
+        name: 'AHS',
+        shares: 79
+    },
+    {
+        name: 'Marvel',
+        shares: 50
+    },
+]
