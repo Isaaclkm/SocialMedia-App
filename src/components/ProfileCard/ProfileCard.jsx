5803932388147c409849d6084733d8915bf758e4
Isaaclkm <isaacfelixmatos@gmail.com>
2022-11-08 22:13:14 -0500

    profile paga

--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -4,7 +4,12 @@ import Profile from '../../img/profileImg.jpg'
 import './ProfileCard.css'
 
 const ProfileCard = () => {
+
+
+  const ProfilePage = true;
+
   return (
+    
     <div className='ProfileCard'>
         <div className="ProfileImages">
             <img src={Cover} alt="" />
@@ -28,14 +33,22 @@ const ProfileCard = () => {
                     <span>1</span>
                     <span>Followers</span>
                 </div>
-                
+                {ProfilePage && (
+                    <>
+                    <div className="vl">
+
+                    </div>
+                    <div className="follow">
+                        <span>3</span>
+                        <span>Posts</span>
+                    </div>
+                    </>
+                )}
             </div>
             <hr/>
         </div>
-
-        <span>
-            My Profile
-        </span>
+           {ProfilePage ? "": <span>My Profile</span>}             
+    
     </div>
   )
 }
