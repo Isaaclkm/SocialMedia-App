c31cdc8517598fe6e35213d040267a930256770e
Isaaclkm <isaacfelixmatos@gmail.com>
2022-12-19 22:39:01 -0500

    hl

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -19,7 +19,7 @@ const Auth = () => {
         setData({...data, [e.target.name]: e.target.value})
     };
 
-    //handle submit
+    //handle submit someting 
     const handleSubmit = (e) =>{
         e.preventDefault();
 
